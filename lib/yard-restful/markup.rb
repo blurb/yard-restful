70351c7220cb7904e275fd41c25b4e037eba8383
Brian Williams <bwilliams@blurb.com>
2013-06-05 09:40:15 -0700

    Fix collision with Markup.rb in blurby

--- lib/yard-restful/markup.rb
+++ lib/yard-restful/markup.rb
@@ -1,18 +1,16 @@
 # create a wrapper around redcarpet to turn on table parsing
-module Markup
-  module Blurb
-    class CustomMarkupProvider
-      attr_accessor :provider
+module Blurb
+  class CustomMarkupProvider
+    attr_accessor :provider
 
-      def initialize(text)
-        self.provider = RedcarpetCompat.new(text,
-                             :no_intraemphasis, :gh_blockcode,
-                             :tables,:fenced_code, :autolink)
-      end
+    def initialize(text)
+      self.provider = RedcarpetCompat.new(text,
+                           :no_intraemphasis, :gh_blockcode,
+                           :tables,:fenced_code, :autolink)
+    end
 
-      def to_html
-        provider.to_html
-      end
+    def to_html
+      provider.to_html
     end
   end
 end
@@ -23,7 +21,7 @@ module YARD
     # Helper methods for loading and managing markup types.
     module MarkupHelper
       MARKUP_PROVIDERS[:markdown].unshift( { :lib => :redcarpet, 
-        :const => 'Markup::Blurb::CustomMarkupProvider'} )
+        :const => 'Blurb::CustomMarkupProvider'} )
     end
   end
 end
